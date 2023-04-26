c564072f00859cb473d7432354c146de989c2686
ArturoAlmagroMendez <almagromendezarturo@gmail.com>
2023-04-26 08:55:22 +0200

    faqs section to web components

--- src/faqs.js
+++ src/faqs.js
@@ -5,6 +5,32 @@ class Faqs extends HTMLElement {
       this.render();
 
     }
+    toggleFaqs() {
+      const dropDownItems = this.shadowRoot.querySelectorAll('.faqs-item .plus-minus-toggle');
+      const invisibleItems = this.shadowRoot.querySelectorAll('.faqs-item .answer');
+  
+      dropDownItems.forEach(dropDownItem => dropDownItem.addEventListener('click', () => {
+        let dropDownParent = dropDownItem.closest('.faqs-item');
+        let icons = dropDownParent.querySelectorAll('.plus-minus-toggle');
+        let invisibleItem = dropDownParent.querySelector('.answer');
+        let activeItem = dropDownParent.querySelector('.answer.active');
+        icons.forEach(icon => icon.classList.toggle('active'));
+  
+        if (activeItem && activeItem !== invisibleItem) {
+          activeItem.classList.toggle('active');
+        }
+        if (!invisibleItem.classList.contains('active')) {
+          invisibleItem.classList.add('active');
+          setTimeout(() => invisibleItem.classList.add('fade-in'), 50);
+        } else {
+          invisibleItem.classList.remove('fade-in');
+          setTimeout(() => invisibleItem.classList.remove('active'), 400);
+        }
+      }));
+    
+    }
+  
+    
     render() {
       // Método que genera el código HTML del componentea
       this.shadowRoot.innerHTML = `
@@ -191,9 +217,8 @@ class Faqs extends HTMLElement {
       `;
     }
     connectedCallback() {
-      // Método que se ejecuta cuando el componente se agrega al DOM
+      this.toggleFaqs();
     }
-  
   
     disconnectedCallback() {
       // Método que se ejecuta cuando el componente se elimina del DOM
